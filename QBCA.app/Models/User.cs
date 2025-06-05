472022c6240b0e1b1b53679c9fa011b7edab89ce
Nguyễn Phông 049205016216 <phongn6216@ut.edu.vn>
2025-06-05 12:18:35 +0700

    Add files via upload

---
+++ QBCA.app/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using QBCA.Dataccess.Entities;
+
+namespace QBCA.app.Models;
+//Quảng lý người dùng 
+public class User
+{
+    public string FullName {get; set;}
+    
+    public string Email {get; set;}
+    
+    public string Id {get; set;} 
+    
+    public string Role {get; set;}
+    
+    
+    public ICollection<Notification> Notifications {get; set;}
+    public ICollection<Task> tasks  {get; set;}
+    
+}
