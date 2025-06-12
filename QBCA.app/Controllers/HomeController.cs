e6f7915d3fddad2da64b08ef0e532e9bcc9288b5
Nguyễn Phông 049205016216 <phongn6216@ut.edu.vn>
2025-06-13 02:35:31 +0700

    Update HomeController.cs

--- QBCA.app/Controllers/HomeController.cs
+++ QBCA.app/Controllers/HomeController.cs
@@ -6,19 +6,32 @@ namespace QBCA.app.Controllers;
 
 public class HomeController : Controller
 {
+    LoginModel db = new LoginModel();
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
-
+    //Trả về trực tiếp dử liệu
+    //Http Get/Home/Index
     public IActionResult Index()
     {
         return View();
     }
-
+    //Http Get/Home/Pricavy
     public IActionResult Privacy()
+    {
+        return View();
+    }
+    //Http Get/Home/Dangky
+    public IActionResult Dangky()
+    {
+        return View();
+    }
+    //Http Post/Home/Dangky
+    [HttpPost]
+     public IActionResult Dangky(User user)
     {
         return View();
     }
