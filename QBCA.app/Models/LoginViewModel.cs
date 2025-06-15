4c7ec769ae735e43cfa169991d2738ddeb6451be
Nguyễn Phông 049205016216 <phongn6216@ut.edu.vn>
2025-06-16 05:14:56 +0700

    Add files via upload

---
+++ QBCA.app/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WeBQBCA.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username {  get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+    }
+}
