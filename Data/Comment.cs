a8beccebff0b9cfed817d26076adfb21748d1b91
Nguyễn Phông 049205016216 <phongn6216@ut.edu.vn>
2025-06-21 16:49:42 +0700

    Add files via upload

---
+++ Data/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeBQBCA.Data;
+
+public partial class Comment
+{
+    public int CommentsId { get; set; }
+
+    public int QuestionId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string? Content { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public bool? IsReviewer { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
