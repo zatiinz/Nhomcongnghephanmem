abd335d5d55f0bcbf20e1f90bc2ad46d422deb4d
Kevin Hòa <hoatm8543@ut.edu.vn>
2025-06-21 16:39:52 +0700

    QuestionController.cs

---
+++ QBCA.app/Controllers/QuestionController.cs
@@ -0,0 +1,12 @@
+using System.Configuration;
+using System.Diagnostics;
+using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using WeBQBCA.Data;
+using WeBQBCA.Models;
+namespace WeBQBCA.Controllers
+{
+    public class QuestionController
+    {
+    }
+}
